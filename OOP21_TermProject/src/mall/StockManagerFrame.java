53cec1faf40751d9274c1408d521e10053f430d4
soominjeong99 <79966717+soominjeong99@users.noreply.github.com>
2021-06-15 01:00:25 +0900

    StockManagerFrame 내부 Frame 추가
    StockRegister, StockManaging, StockSearch Frame 추가

--- OOP21_TermProject/src/mall/StockManagerFrame.java
+++ OOP21_TermProject/src/mall/StockManagerFrame.java
@@ -6,17 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.FlowLayout;
-import javax.swing.JTextField;
-import javax.swing.WindowConstants;
 
-import java.awt.Font;
-import javax.swing.JLabel;
-
-public class ManagerFrame extends JFrame {
+public class StockManagerFrame extends JFrame {
 
 	private JPanel contentPane;
 
@@ -27,7 +22,7 @@ public class ManagerFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ManagerFrame frame = new ManagerFrame();
+					StockManagerFrame frame = new StockManagerFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,42 +34,41 @@ public class ManagerFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public ManagerFrame() {
-		setTitle("관리자 메인 메뉴창"); //title
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public StockManagerFrame() {
 		
+		setTitle("재고 관리창"); //title
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("고객 관리");
-		btnNewButton.setBounds(71, 143, 137, 50);
-		contentPane.add(btnNewButton);
-		
-		//고객 관리 페이지로 이동
+		JButton btnNewButton = new JButton("상품 등록");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			UserListFrame.main(null);
-						}
+				StockRegister.main(null);
+			}
 		});
+		btnNewButton.setBounds(172, 39, 118, 44);
+		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("재고 관리");
-		btnNewButton_1.setBounds(240, 143, 137, 50);
-		contentPane.add(btnNewButton_1);
-		//재고 관리 페이지로 이동
+		JButton btnNewButton_1 = new JButton("상품 관리");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			StockManagerFrame.main(null);
-						}
+				StockManaging.main(null);
+			}
 		});
+		btnNewButton_1.setBounds(172, 107, 118, 44);
+		contentPane.add(btnNewButton_1);
 		
-		JLabel lblNewLabel = new JLabel("관리자 모드");
-		lblNewLabel.setFont(new Font("굴림", Font.PLAIN, 20));
-		lblNewLabel.setBounds(167, 68, 113, 40);
-		contentPane.add(lblNewLabel);
-		
-	
+		JButton btnNewButton_2 = new JButton("상품 검색");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				StockSearch.main(null);
+			}
+		});
+		btnNewButton_2.setBounds(172, 173, 118, 44);
+		contentPane.add(btnNewButton_2);
 	}
 }
