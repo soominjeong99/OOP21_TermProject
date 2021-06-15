34e1e2ce45bed9b28011bfd66f29629e6b5c5362
주은 <00-10-21@naver.com>
2021-06-15 18:07:17 +0900

    CartFrame 구현 방법 수정, CartFrame3 추가 (미완성)
    CartFrame - Table로 구현 / java.lang.ArrayStoreException 에러
    CartFrame3 - console에 println하는 것으로 구현 / NullPointerException 에러

--- OOP21_TermProject/src/mall/CartFrame.java
+++ OOP21_TermProject/src/mall/CartFrame.java
@@ -1,42 +1,58 @@
 package mall;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import java.io.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.text.DecimalFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+ 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableCellRenderer;
 
 public class CartFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 
-	public void Table() throws FileNotFoundException {
+	public CartFrame() throws FileNotFoundException {
+		
+		String file = "Cart.txt";
+		
 		String[] columns = new String[] {
-				"No.", "이름", "갯수", "가격", "총 가격"
+				"No.", "이름", "재고수량", "갯수", "가격", "총 가격"
 		};
+		
+		Scanner sc = new Scanner(new FileReader(file)); // cart database에서 읽어오기
 
-		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<Cart> arrayCart = new ArrayList<Cart>();
 
-		Scanner sc = new Scanner(new File("Cart.txt"), "UTF-8"); // cart database에서 읽어오기
-
-		while (sc.hasNext())
+		while (sc.hasNextLine())
 		{
-			list.add(sc.nextLine().split(" "));
+			String temp = sc.nextLine();
+			String[] split = temp.split("/");
+			
+			int id = Integer.parseInt(split[0]);
+			String name = split[1];
+			int num = Integer.parseInt(split[2]);
+			int buyNumber = Integer.parseInt(split[3]);
+			int perPrice = Integer.parseInt(split[4]);
+			int tolPrice = Integer.parseInt(split[5]);
+			
+			Cart c = new Cart(id, name, num, buyNumber, perPrice, tolPrice);
+			
+			arrayCart.add(c);
 		}
 
 		sc.close();
 
-		String[][] stringArray = list.stream().toArray(String[][]::new);
+		String[][] stringArray = arrayCart.stream().toArray(String[][]::new);
 
-		for (String[] array : stringArray)
-		{
-			array[3] = new DecimalFormat("#,##0").format(Integer.parseInt(array[3]));
-		}
-		
 		JTable table = new JTable(stringArray, columns);
 
 		this.add(new JScrollPane(table));
@@ -47,12 +63,15 @@ public class CartFrame extends JFrame {
 		this.setVisible(true);
 	}
 
-	public static void main(String[] args)
-	{ 
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				new CartFrame();
+				try {
+					CartFrame cartFrame = new CartFrame();
+					cartFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
 	}
