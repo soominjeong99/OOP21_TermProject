64ad9602046e86f72cc3c8c5f2423456231910e5
001021 <84309420+001021@users.noreply.github.com>
2021-06-16 00:45:23 +0900

    CustomerManager 배열을 리스트로 수정

--- OOP21_TermProject/src/mall/CustomerManager.java
+++ OOP21_TermProject/src/mall/CustomerManager.java
@@ -3,9 +3,72 @@ package mall;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 //Managing Customer
 public class CustomerManager implements Manager {
 	
+	ArrayList<Customer> customers;
+	
+	public CustomerManager()
+	{
+		customers = new ArrayList<>();
+	}
+	
+	@Override
+	public void add(Object o)
+	{
+		customers.add((Customer) o);
+	}
+	
+	@Override
+	public void remove(int id)
+	{
+		Customer temp;
+		
+		for (int i = 0; i < customers.size(); i++)
+		{
+			temp = customers.get(i);
+			if (temp.getId() == id)
+			{
+				customers.remove(temp);
+				break;
+			}
+		}
+	}
+	
+	@Override
+	public void modify(int id, Object o)
+	{
+		Customer temp;
+		
+		for (int i = 0; i < customers.size(); i++)
+		{
+			temp = customers.get(i);
+			if (temp.getId() == id)
+			{
+				customers.remove(temp);
+				customers.add((Customer) o);
+				break;
+			}
+		}
+	}
+	
+	@Override
+	public void display()
+	{
+		System.out.println("\n Customers present are: ");
+		Customer temp;
+		
+		for (int i = 0; i < customers.size(); i++)
+		{
+			temp = customers.get(i);
+			System.out.println(temp.toString());
+		}
+	}
+}
+
+
+/**
 	ArrayList<Customer> customerArray = new ArrayList<Customer>();// Array of goods
 	int customerCount = 0;
 
@@ -31,7 +94,7 @@ public class CustomerManager implements Manager {
 
 			}
 		} catch (Exception e) {
-			throw new Exception("물품 없음");
+			throw new Exception("臾쇳뭹 �뾾�쓬");
 		}
 
 	}
@@ -49,7 +112,7 @@ public class CustomerManager implements Manager {
 			}
 
 		} catch (Exception e) {
-			throw new Exception("물품 없음");
+			throw new Exception("臾쇳뭹 �뾾�쓬");
 		}
 
 	}
@@ -63,3 +126,4 @@ public class CustomerManager implements Manager {
 	}
 
 }
+*/
